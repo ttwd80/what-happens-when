40b14f7e9964b6fbe631bbf7fc349c04f19cec96
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-06-13 00:38:55 +1000

    remove chrome deb after install

--- docker/install-script/install-chrome.sh
+++ docker/install-script/install-chrome.sh
@@ -3,3 +3,4 @@ wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 apt-get install libgtk-4-1 libcurl4 xdg-utils fonts-liberation \
   libasound2 libgbm1 libnspr4 libnss3 -qq
 dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
