a9d699d1a78bde8a793afa0e4fb79e2b2b7975e7
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-06-13 00:36:56 +1000

    fix: chrome driver version can be different

--- docker/install-script/install-chrome-driver-exact-version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wget https://chromedriver.storage.googleapis.com/$(google-chrome --version | cut -f 3 -d ' ')/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
-chmod a+x chromedriver
-mv chromedriver /usr/local/bin
